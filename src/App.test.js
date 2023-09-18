e4cd5639b3834dec9c75b296da86e2c4b63d96a7
Bryan <bryanj1234@gmail.com>
2023-09-18 17:25:51 -0400

    dsadsada

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import {Main} from './components/Main';
+import { fireEvent, render, screen } from '@testing-library/react';
+import {updateTimes, initializeTimes, Main} from './components/Main';
 import {MemoryRouter} from 'react-router-dom';
 
 test('Renders the BookingForm heading', () => {
@@ -14,4 +14,26 @@ test('Renders the BookingForm heading', () => {
   const headingElement = screen.getByText("Plan your dining experience");
   expect(headingElement).toBeInTheDocument();
 
-})
+  // const dateInput = screen.getByLabelText("Choose date");
+  // expect(dateInput).toBeInTheDocument();
+  // fireEvent.change(dateInput, { target: { value: '2024-01-01' } });
+
+  // expect(updateTimes).toHaveBeenCalledWith({
+  //   score,
+  //   comment: ""
+  // });
+
+  const timesList = initializeTimes();
+  expect(timesList).toEqual([
+    "",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
+
+  console.log(timesList);
+
+});
--- src/components/Main.js
+++ src/components/Main.js
@@ -37,29 +37,29 @@ export const Section = (props) => {
   )
 }
 
+export const updateTimes = () => {
+  return [
+    "",
+    "17:00",
+    "18:00",
+  ];
+}
+
+export const initializeTimes = () => {
+  return [
+    "",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ];
+}
+
 export const Main = () => {
 
   const GC = useGC();
-
-  const updateTimes = () => {
-    return [
-      "",
-      "17:00",
-      "18:00",
-    ];
-  }
-
-  const initializeTimes = () => {
-    return [
-      "",
-      "17:00",
-      "18:00",
-      "19:00",
-      "20:00",
-      "21:00",
-      "22:00",
-    ];
-  }
 
   function dateChangeReducer(visitDate, _) {
     return {availableTimes: updateTimes()}
